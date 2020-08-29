08853a4816c648cfb946e3647054e02d3cb5f130
kntaka <knt.takahashii@gmail.com>
2020-08-29 16:46:47 +0900

    [WIP]add parser test

---
+++ parser/parser_test.go
@@ -0,0 +1,47 @@
+package parser
+
+import (
+	"testing"
+
+	"github.com/knwoop/monkey/ast"
+	"github.com/knwoop/monkey/lexer"
+)
+
+func TestLetStatements(t *testing.T) {
+	input := `
+let x = 5;
+let y = 10;
+let foobar = 838383;
+`
+	l := lexer.New(input)
+	p := New(l)
+
+	program := p.ParseProgram()
+	if program == nil {
+		t.Fatal("ParseProgram() returned nil")
+
+	}
+	if len(program.Statements) != 3 {
+		t.Fatalf("program.Statements does not contain 3 statements. got=%d",
+			len(program.Statements))
+	}
+
+	tests := []struct {
+		expectedIdentifier string
+	}{
+		{"x"},
+		{"y"},
+		{"foobar"},
+	}
+
+	for i, tt := range tests {
+		stmt := program.Statements[i]
+		if !testLetStatement(t, stmt, tt.expectedIdentifier) {
+			return
+		}
+	}
+}
+
+func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
+	return true
+}
